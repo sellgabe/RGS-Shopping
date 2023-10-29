22ce0359986e14a4e4652f6449e433ca4114126a
Russell San Pedro <sellgabe23@gmail.com>
2023-10-29 22:17:52 +0800

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/RGS-Shopping/">
+    <BrowserRouter basename="/rgs-shopping/">
       <AppProvider>
         <App />
       </AppProvider>
